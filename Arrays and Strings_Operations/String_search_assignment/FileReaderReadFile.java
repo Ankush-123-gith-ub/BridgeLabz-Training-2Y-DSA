c3a457bb25354a35240222c9386aeb7a6c4d7144
Ankush <ffp900h@gmail.com>
2026-04-29 22:19:59 +0530

    [Ankush].add Arrays_and_Strings_Questions

---
+++ Arrays and Strings_Operations/String_search_assignment/FileReaderReadFile.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class FileReaderReadFile{
+public static void main(String[] args) throws Exception{
+BufferedReader br=new BufferedReader(new FileReader("test.txt"));
+String line;
+while((line=br.readLine())!=null){
+System.out.println(line);
+}
+br.close();
+}
+}
